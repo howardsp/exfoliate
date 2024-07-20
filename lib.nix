dca8479d414dfbb092675037fc8d45854e92c112
Howard Spector <howardsp@gmail.com>
2024-07-20 11:04:28 -0400

    update

--- lib.nix
+++ lib.nix
@@ -4,28 +4,28 @@
 }:
 
   let
-
     nixpkgs = null;
-    createSystem = { host, username ? "howardsp", fullname ? "Howard Spector", system ? "x86_64-linux",  allowUnfree ? true }: nixpkgs.lib.nixosSystem {
+    createSystem = { host, username ? "howardsp", fullname ? "Howard Spector", system ? "x86_64-linux",  allowUnfree ? true }: 
+      let      
+      in
+        nixpkgs.lib.nixosSystem {
+          inherit system;
 
-      inherit system;
-
-      modules = [
-          (./hosts/${host}.nix)
-          (./hardware/hardware-${host}.nix)
-          (lib.mkIf allowUnfree {nixpkgs.config.allowUnfree = true;})
-          home-manager.nixosModules.home-manager {
-            home-manager.useUserPackages = true;
-            home-manager.useGlobalPkgs = true;
-            home-manager.users.howardsp = (./users/${username}-${host}.nix);
-            home-manager.extraSpecialArgs = { inherit  host username fullname; };
-          }
-        ];
-        specialArgs = { inherit  host username fullname  home-manager;};
-      };
-
-    lib = {
-      inherit createSystem;
+          modules = [
+              (./hosts/${host}.nix)
+              (./hardware/hardware-${host}.nix)
+              (lib.mkIf allowUnfree {nixpkgs.config.allowUnfree = true;})
+              home-manager.nixosModules.home-manager {
+                home-manager.useUserPackages = true;
+                home-manager.useGlobalPkgs = true;
+                home-manager.users.howardsp = (./users/${username}-${host}.nix);
+                home-manager.extraSpecialArgs = { inherit  host username fullname; };
+              }
+            ];
+            specialArgs = { inherit  host username fullname  home-manager;};
+          };
+  lib = {
+    inherit createSystem;
     };
   in
   lib
