675073e9811166f3bd9ab793b6febf37095fbcb4
Howard Spector <howardsp@gmail.com>
2024-07-21 10:17:38 -0400

    update

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
   # Externally extensible flake systems. See <https://github.com/nix-systems/nix-systems>.
   #inputs.systems.url = "github:nix-systems/default";
 
-  outputs = { self, system }: {
+  outputs = { self }: {
     createSystem = import ./lib.nix;
     
 
